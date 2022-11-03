9654d725367fed3096a689b56ad8b6c3cab481c7
eceulkuuu <107754176+eceulkuuu@users.noreply.github.com>
2022-11-04 00:44:41 +0300

    Create toFahrenheit.java

---
+++ src/org/javaturk/oopj/TemperatureConverter.java/toFahrenheit.java
@@ -0,0 +1,21 @@
+package org.javaturk.oopj.TemperatureConverter;
+
+public class ToFahrenheit implements Calculator {
+
+	@Override
+	public Unit calculate(Unit value) {
+
+		double newValue=0;
+		
+		if(value instanceof Kelvin) 
+    {
+			newValue=((value.getValue()- 273.15)*9/5 + 32);
+		}
+		else if(value instanceof Celcius) 
+    {
+			newValue=(value.getValue()*(9/5)+32);
+		}
+		
+		Unit x= new Fahrenheit(newValue);
+		return x;
+	}
