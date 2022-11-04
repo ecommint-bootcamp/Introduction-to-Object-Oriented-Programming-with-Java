86908e8d135d696f031ccc98f80d0ba5c65141ae
eceulkuuu <107754176+eceulkuuu@users.noreply.github.com>
2022-11-04 20:42:25 +0300

    Course.java

--- src/org/javaturk/oopj/ch08/university/Course.java
+++ src/org/javaturk/oopj/ch08/university/Course.java
@@ -11,11 +11,79 @@ import java.util.Date;
  *      </p>
  */
 public class Course {
-	String code;
-    String name;
-    Department department;
-    Professor teacher;
-    Student[] students;
-    String[] days;
-    Date time;
-}
+    private String code;
+    private String name;
+    private  Department department;
+    private  Professor teacher;
+    private  Student[] students;
+    private  String[] days;
+    private  Date time;
+
+    public Course(String code, String name, Department department, Professor teacher, Student[] students, String[] days, Date time) {
+        this.code = code;
+        this.name = name;
+        this.department = department;
+        this.teacher = teacher;
+        this.students = students;
+        this.days = days;
+        this.time = time;
+    }
+
+    public Course() {
+    }
+
+    public String getCode() {
+        return code;
+    }
+
+    public void setCode(String code) {
+        this.code = code;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Department getDepartment() {
+        return department;
+    }
+
+    public void setDepartment(Department department) {
+        this.department = department;
+    }
+
+    public Professor getTeacher() {
+        return teacher;
+    }
+
+    public void setTeacher(Professor teacher) {
+        this.teacher = teacher;
+    }
+
+    public Student[] getStudents() {
+        return students;
+    }
+
+    public void setStudents(Student[] students) {
+        this.students = students;
+    }
+
+    public String[] getDays() {
+        return days;
+    }
+
+    public void setDays(String[] days) {
+        this.days = days;
+    }
+
+    public Date getTime() {
+        return time;
+    }
+
+    public void setTime(Date time) {
+        this.time = time;
+    }
