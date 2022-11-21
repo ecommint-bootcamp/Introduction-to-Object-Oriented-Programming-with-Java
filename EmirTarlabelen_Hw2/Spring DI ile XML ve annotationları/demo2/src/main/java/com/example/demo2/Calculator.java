3028554170a583c5a1a639f217c5383e5e41a067
Emir Tarlabelen <emirtarlabelen021@gmail.com>
2022-11-21 21:12:10 +0300

    PUT ve DELETE metotlarını GreetingRestSpring’e taşıma

--- EmirTarlabelen_Hw2/Spring DI ile XML ve annotationları/demo2/src/main/java/com/example/demo2/Calculator.java
+++
@@ -1,9 +0,0 @@
-package com.example.demo2;
-
-public interface Calculator {
-    void addFunction(MathFunction function);
-
-    double doCalculation(String functionName, double arg);
-
-    void listMathFunction();
-}
--- EmirTarlabelen_Hw2/Spring DI ile XML ve annotationları/demo2/src/main/java/com/example/demo3/Calculator.java
+++
@@ -1,9 +0,0 @@
-package com.example.demo3;
-
-public interface Calculator {
-    void addFunction(MathFunction function);
-
-    double doCalculation(String functionName, double arg);
-
-    void listMathFunction();
-}
